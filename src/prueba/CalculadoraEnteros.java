e3d07af756b857635e1d39a316f65ad08d675b16
Genesis102018 <43939811+Genesis102018@users.noreply.github.com>
2018-10-07 21:52:20 -0600

    Calculadora

---
+++ src/prueba/CalculadoraEnteros.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Marvin
+ */
+public class CalculadoraEnteros {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+}
---
+++ test/prueba/CalculadoraEnterosTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Marvin
+ */
+public class CalculadoraEnterosTest {
+    
+    public CalculadoraEnterosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of suma method, of class CalculadoraEnteros.
+     */
+    @Test
+    public void testSuma() {
+        System.out.println("suma");
+        int a = 5;
+        int b = 7;
+        CalculadoraEnteros instance = new CalculadoraEnteros();
+        int expResult = 5+7;
+        int result = instance.suma(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
